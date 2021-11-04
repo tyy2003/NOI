b14ebf8255a0a8a2dd932dbc45db38c56cde3452
TanYY <tany2003@qq.com>
2021-11-04 21:05:58 +0800

    Create 05 填空：类型转换2.c

---
+++ 1.2编程基础之变量定义、赋值及转换/05 填空：类型转换2.c
@@ -0,0 +1,26 @@
+/*
+描述
+有两个变量a和b，在执行了如下代码后：
+
+a = 1.000000001;
+b = a;
+printf("%.9f %.9f\n", a, b);
+输出两个数：1.000000001 1.000000000
+
+请问a和b分别是以下哪种类型？
+
+A. bool   B. char   C. short   D. int   E. float   F. double
+
+
+
+输入
+无。
+输出
+一行，包含两个大写字母，分别代表变量a和b的类型标号。中间用一个空格隔开。
+*/
+#include <stdio.h>
+int main()
+{
+    printf("F E")
+    return 0;
+}
