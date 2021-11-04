0ec83d5ab80e98b74896042c1c140c03bde330ed
TanYY <tany2003@qq.com>
2021-11-04 21:05:51 +0800

    Create 03 其他基本数据类型存储空间大小.c

---
+++ 1.2编程基础之变量定义、赋值及转换/03 其他基本数据类型存储空间大小.c
@@ -0,0 +1,19 @@
+/*
+描述
+分别定义bool，char类型的变量各一个，并依次输出它们的存储空间大小（单位：字节）。
+
+输入
+无。
+输出
+一行，两个整数，分别是两个变量的存储空间大小，用一个空格隔开。
+*/
+#include <stdio.h>
+int main()
+{
+    _Bool a;
+    char s;
+
+    printf("%d %d", sizeof(a), sizeof(s));
+
+    return 0;
+}
