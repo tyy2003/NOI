5af03f5483544ed4313f96bbd38d2c57ca690126
TanYY <tany2003@qq.com>
2021-11-07 22:47:08 +0800

    Create 08 打印字符.c

---
+++ 1.2编程基础之变量定义、赋值及转换/08 打印字符.c
@@ -0,0 +1,20 @@
+/*
+描述
+输入一个ASCII码，输出对应的字符。
+
+输入
+一个整数，即字符的ASCII码，保证存在对应的可见字符。
+输出
+一行，包含相应的字符。
+*/
+#include <stdio.h>
+int main()
+{
+    int a;
+    char b;
+    scanf("%d", &a);
+    b = (char)a;
+    printf("%c", b);
+
+    return 0;
+}
