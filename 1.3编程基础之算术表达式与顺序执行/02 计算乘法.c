0958d167d27b4112f7d030f68f8cfd8b4c83edfa
TanYY <tany2003@qq.com>
2021-11-09 23:00:47 +0800

    Create 02 计算乘法.c

---
+++ 1.3编程基础之算术表达式与顺序执行/02 计算乘法.c
@@ -0,0 +1,19 @@
+/*
+描述
+给定3个整数a、b、c，计算表达式(a+b)*c的值。
+
+输入
+输入仅一行，包括三个整数a、b、c, 数与数之间以一个空格分开。
+(－10,000 < a,b,c < 10,000)
+输出
+输出一行，即表达式的值
+*/
+#include <stdio.h>
+int main()
+{
+    int a, b, c, ans;
+    scanf("%d %d %d", &a, &b, &c);
+    ans = (a+b)*c;
+    printf("%d", ans);
+    return 0;
+}
